1b9a620feef18d8108aab0f4de5c3726a1cf353a
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-01-20 14:15:32 -0500

    Initial commit - updated the InstantiateMsg, ExecuteMsg, QuerMsg in msgs.rs

---
+++ src/msg.rs
@@ -0,0 +1,39 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use andromeda_std::{andr_exec, andr_instantiate, andr_query};
+
+#[andr_instantiate]
+#[cw_serde]
+pub struct InstantiateMsg {pub admin: String,
+    pub platform_fee: u128,
+}
+
+#[andr_exec]
+#[cw_serde]
+pub enum ExecuteMsg {
+    ListService {
+        service_id: String,
+        description: String,
+        price: u128,
+        category: String,
+    },
+    PurchaseService {
+        service_id: String,
+        buyer: String,
+    },
+    LeaveReview {
+        service_id: String,
+        rating: u8,
+        feedback: String,
+    },
+    ResolveDispute {
+        service_id: String,
+        resolution: String,
+    },
+}
+
+#[andr_query]
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {
+    
+}
